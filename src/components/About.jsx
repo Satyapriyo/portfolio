697da7502505d3820bb9c981a90ebbc4e42c149e
Satyapriyo Biswas <satyapriyoak47@gmail.com>
2022-09-20 23:02:56 +0530

    making some changes 🔨🔨

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,25 +9,25 @@ const About = () => {
       </div>
       <Marquee pauseOnHover="true" speed="40">
         <div className="text-white font-bold flex mt-[20vh] ">
-          <div className="section bg-orange-400 w-48 p-4 text-center my-[2vh] hover:scale-105 duration-200 rounded mx-20">
+          <div className="section bg-orange-400 w-48 h-32 pt-10 text-center my-[2vh] hover:scale-105 duration-200 rounded mx-20">
             <div>Html</div>
           </div>
-          <div className="section bg-sky-400 w-48 p-4 text-center my-[2vh] hover:scale-105 duration-200 rounded mx-20">
+          <div className="section bg-sky-400 w-48 h-32 pt-10 text-center my-[2vh] hover:scale-105 duration-200 rounded mx-20">
             <div>Css</div>
           </div>
-          <div className="section bg-yellow-400 w-48 text-center p-4 my-[2vh] hover:scale-105 duration-200 rounded mx-20">
+          <div className="section bg-yellow-400 w-48 h-32 text-center pt-10 my-[2vh] hover:scale-105 duration-200 rounded mx-20">
             <div>Javascript</div>
           </div>
-          <div className="section bg-blue-400 w-48 text-center p-4 my-[2vh] hover:scale-105 duration-200 rounded mx-20">
+          <div className="section bg-blue-400 w-48 h-32 text-center pt-10 my-[2vh] hover:scale-105 duration-200 rounded mx-20">
             <div>ReactJs</div>
           </div>
-          <div className="section bg-green-500 w-48 text-center p-4 my-[2vh] hover:scale-105 duration-200 rounded mx-20">
+          <div className="section bg-green-500 w-48 h-32 text-center pt-10 my-[2vh] hover:scale-105 duration-200 rounded mx-20">
             <div>Tailwind Css</div>
           </div>
-          <div className="section bg-violet-500 w-48 text-center p-4 my-[2vh] hover:scale-105 duration-200 rounded mx-20">
+          <div className="section bg-violet-500 w-48 h-32 text-center pt-10 my-[2vh] hover:scale-105 duration-200 rounded mx-20">
             <div>Bootstrap</div>
           </div>
-          <div className="section bg-slate-500 w-48 text-center p-4 my-[2vh] hover:scale-105 duration-200 rounded mx-20">
+          <div className="section bg-slate-500 w-48 h-32 text-center pt-10 my-[2vh] hover:scale-105 duration-200 rounded mx-20">
             <div>Git & GitHub</div>
           </div>
         </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,33 @@
-import React from "react";  
-import "../index.css"
+import React from "react";
+import "../index.css";
 const Projects = () => {
   return (
     <>
-
-      <div className="w-28 bg-slate-600 mx-auto p-2 rounded mt-20 text-lg  font-extrabold text-white">
+      <div className="w-28 bg-slate-600 mx-auto my-16 p-2 rounded mt-20 text-lg  font-extrabold text-white">
         Projects
+      </div>
+      <div className="md:w-64 w-48 lg:w-96 bg-special mx-auto p-4 rounded hover:scale-125 duration-700 ease-in-out">
+        <div className=" p-2 sm:text-md text-xl text-center font-bold">
+          Twitter Login page clone
+        </div>
+        <div className="mx-auto w-32 sm:text-sm text-center text-md font-bold">
+          front end{" "}
+        </div>
+        <div className="h-48 m-2 p-7 text-white ">
+          This is a Twitter login page clone (frontend)
+          <br />
+          <div className="flex mt-14 hover: text-blact">
+            
+            <div>
+              <a className="p-4" href="https://satyapriyobiswas.netlify.app/">link</a>
+            </div>
+            <a href="https://github.com/Satyapriyo/TwitterLogIn"><i className="fa-brands fa-github p-2 cursor-pointer"></i></a> 
+          </div>
+        </div>
       </div>
       {/* <div className="w-48 p-4 mx-auto mt-32 text-3xl font-mono">
         Comming Soon ....
       </div> */}
-      
     </>
   );
 };
